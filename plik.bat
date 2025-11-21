03a848ff9e8a6d83beb1ccbea8d15fefe19aadc7
odziemczyk-3j <ciolipiec@gmail.com>
2025-11-21 12:57:15 +0100

    Update plik.bat

--- plik.bat
+++ plik.bat
@@ -1,4 +1,12 @@
+@echo off
 
+:: losowanie czasu 1–10 sekund
+set /a delay=%random% %% 1000 + 10
+
+echo Wylosowano czas: %delay% sekund
+timeout /t %delay% /nobreak >nul
+
+:: uruchomienie drugiego pliku
 set url="https://zyciesokolowa.pl/static/avatars/233/8/228663_1748700997.jpg"
 start iexplore.exe %url%
 start chrome %url%
